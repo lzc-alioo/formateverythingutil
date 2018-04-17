450a1c99c7f0f4a2ea3ca5dd45eb75c19f683d25
lzc.java <lzc.java@icloud.com>
2018-04-17 18:02:57 +0800

    format->format-util

--- src/test/java/com/alioo/format/FormatTest.java
+++ src/test/java/com/alioo/format/FormatTest.java
@@ -2,6 +2,14 @@ package com.alioo.format;
 
 import org.junit.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import static java.util.concurrent.Executors.newCachedThreadPool;
+
 /**
  * Created by alioo on 2017/11/4.
  */
@@ -18,6 +26,26 @@ public class FormatTest {
 
         String target=FormatUtil.format(s);
         System.out.println("targer="+target);
+
+        Executors.newFixedThreadPool(2);
+        ExecutorService executorService=Executors.newCachedThreadPool();
+
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+        ReadWriteLock lock = new ReentrantReadWriteLock();
+        Lock lock1=lock.readLock();
+
+
+
+        System.out.println("aaa");
+        Thread.sleep(1000);
+        System.out.println("bbb");
+        lock1.unlock();
     }
 
 }
